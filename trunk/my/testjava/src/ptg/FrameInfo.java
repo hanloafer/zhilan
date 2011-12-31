894d1a9592d8b9adfe25b1cc638e4db05c24befe
zhcolleen@gmail.com <zhcolleen@gmail.com@f16c1602-99a8-c953-a4e8-3898b561e8a6>
2011-12-31 10:01:52 +0000

    gfx 解析

---
+++ trunk/my/testjava/src/ptg/FrameInfo.java
@@ -0,0 +1,12 @@
+package ptg;
+
+public class FrameInfo {
+	public int iFrame;
+	public int iOffsetX;
+	public int iOffsetY;
+	public int nRawWidth;
+	public int nRawHeight;
+	
+	public int drawOfsX;
+	public int drawOfsY;
+}
