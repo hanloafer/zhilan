c20453bcee8187d665b8192988aa13ab09b66076
hanloafer@gmail.com <hanloafer@gmail.com@f16c1602-99a8-c953-a4e8-3898b561e8a6>
2012-04-06 10:11:10 +0000

    测试

--- my/testjava/src/PngToGFX.java
+++ my/testjava/src/PngToGFX.java
@@ -21,7 +21,7 @@ public class PngToGFX implements FilenameFilter {
 			File png = new  File(p.getAbsolutePath() + "\\" + string);
 //			System.out.println(png.getAbsolutePath());
 			ptg.pngCutAlphaRect(png);
-			break;
+//			break;
 		}
 //		int t = 0xffffffff;
 //		t = t>> 24;
@@ -31,15 +31,7 @@ public class PngToGFX implements FilenameFilter {
 //		System.out.println(t);
 	}
 	
-	private void pngCutAlphaRect(File png){
-		BufferedImage img = null;
-		try {
-			img = ImageIO.read(png);
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		
+	private BufferedImage imgCutAlphaRect(BufferedImage img){
 		int w = img.getWidth();
 		int h = img.getHeight();
 		int top=-1, bottom=-1, left=-1, right=-1;
@@ -107,14 +99,55 @@ public class PngToGFX implements FilenameFilter {
 				break;
 			}
 		}
-		System.out.println("r c");
 		int newW = right - left + 1;
 		int newH = bottom - top + 1;
-		int [] rgbArray  = new int[newW * newH]; 
-		img.getRGB(left, top, newW, newH, rgbArray, 0, newW);
+		//int [] rgbArray  = new int[newW * newH]; 
+		//img.getRGB(left, top, newW, newH, rgbArray, 0, newW);
+		int [] rgbArray  = new int[img.getWidth() * img.getHeight()]; 
 		
-		BufferedImage newimg = new BufferedImage(newW, newH, BufferedImage.TYPE_INT_ARGB);
-		newimg.setRGB(0, 0, newW, newH, rgbArray, 0, newW);
+		img.getRGB(0, 0, img.getWidth(), img.getHeight(), rgbArray, 0, img.getWidth());
+		
+		BufferedImage newimg = new BufferedImage(img.getWidth(), img.getHeight(), BufferedImage.TYPE_INT_RGB );
+		//newimg.setRGB(0, 0, newW, newH, rgbArray, 0, newW);
+		newimg.setRGB(0, 0, img.getWidth(), img.getHeight(), rgbArray, 0, img.getWidth());
+		return newimg;
+	}
+	
+	
+	
+	private BufferedImage mergeBitmapData(BufferedImage [] bitmapDatas){
+		int w = 0,h = 0;
+		for(int i=0; i<bitmapDatas.length; i++){
+			BufferedImage tmp = bitmapDatas[i];
+			w += tmp.getWidth();
+			if(tmp.getHeight() > h){
+				h = tmp.getHeight();
+			}
+		}
+		int offsetX = 0;
+		BufferedImage newimg = new BufferedImage(w, h, BufferedImage.TYPE_INT_RGB );
+		for(int i=0; i<bitmapDatas.length; i++){
+			BufferedImage tmp = bitmapDatas[i];
+			int [] rgbArray = tmp.getRGB(0, 0, tmp.getWidth(), tmp.getHeight(), null, 0, tmp.getWidth());
+			newimg.setRGB(offsetX, 0, tmp.getWidth(), tmp.getHeight(), rgbArray, 0, tmp.getWidth());
+			
+			offsetX += tmp.getWidth();
+		}
+		return newimg;
+	}
+	
+	private void pngCutAlphaRect(File png){
+		BufferedImage img = null;
+		try {
+			img = ImageIO.read(png);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		
+		
+		BufferedImage newimg = imgCutAlphaRect(img);
 		try {
 			ImageIO.write(newimg, "png", new File("src/cut/" + png.getName()));
 		} catch (IOException e) {
